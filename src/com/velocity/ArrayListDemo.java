4af8484e337d22f941eee7b1820fb18ad97b363a
rupeshsadavarte <rupeshsadavarte26@gmail.com>
2022-12-26 20:21:03 +0530

    first Commit

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("Rupesh");
+		System.out.println(list);
+	}
+
+}
